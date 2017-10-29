dfca09c9adcd0b5e6a9e4f789edde560d1857271
varun <gargvarun455@gmail.com>
2017-10-29 16:48:06 +0530

    minor changes

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -7,31 +7,12 @@ export class AuthService {
 
   constructor() { }
 
-  authorizeUser(username: String, password: String): boolean {
-    /* if (username == "admin" && password == "admin") {
-      return true;
-    }
-    else {
-      return false;
-    } */
-    
-  }
-
   getLoggedInStatus(): boolean {
     return this.isLoggedIn;
   }
 
   setLoggedInStatus(status: boolean) {
     this.isLoggedIn = status;
-  }
-
-  getLoggedInUser(): String {
-
-    
-  }
-
-  setLoggedInUser(username: String) {
-
   }
 
 }
